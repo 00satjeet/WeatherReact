bdecd734fb51f4576f4557b34e873f118ad237f1
satjeet <arturo.alfaro87@gmail.com>
2017-02-27 05:14:35 -0300

    localhost funcionando

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const PORT= process.env.PORT || 3000;
 
 
 app.use(function(req,res,next){
-	if(req.headers['x-forwarded-proto']==='http'){
-		next();
-	}else{
+	if(req.headers['x-forwarded-proto']==='https'){
 		res.redirect('http://'+req.hostname+req.url);
+
+	}else{
+		next();
+		
 	}
 });
 //indico la carpeta que quiero hacer publica
