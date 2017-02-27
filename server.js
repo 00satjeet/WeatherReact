0e01ea04a4fc712039bbd76fa1ea075021112175
satjeet <arturo.alfaro87@gmail.com>
2017-02-27 00:48:18 -0300

    preparacion para heroku

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ var express=require('express');
 //Crear la app
 
 var app=express();
+const PORT= process.env.PORT || 3000;
 
+
+app.use(function(req,res,next){
+	if(req.headers['x-forwarded-proto']==='http'){
+		next();
+	}else{
+		res.redirect('http://'+req.hostname+req.url);
+	}
+});
 //indico la carpeta que quiero hacer publica
 app.use(express.static('public'));
 
 
 //comenzar el server
-app.listen(3000,function(){
-	console.log('subiendo el server exprres en el puerto 3000');
+app.listen(PORT,function(){
+	console.log('subiendo el server exprres en el puerto 3000 o al puerto:'+ PORT);
 
 });
